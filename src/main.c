65d5770ef72ea5f1f5f468092c659e7a3de4fe18
Uniolo1 <89284448+Uniolo1@users.noreply.github.com>
2026-09-21 19:59:12 -0400

    Reorganized 'main.c'
    Signed-off-by: Uniolo1 <89284448+Uniolo1@users.noreply.github.com>

--- src/main.c
+++ src/main.c
@@ -6,6 +6,58 @@
 
 #include "parse.h"
 
+int uwuify(void);
+size_t sof_getline(char **lineptr, size_t *n, FILE *stream);
+
+int main(int argc, char *argv[]) {
+	char *text = NULL;
+	switch (argc) {
+		case 0:
+		case 1:
+			return uwuify();
+			break;
+		case 2:
+			text = uwuify_string(argv[1]);
+			if (text == NULL) return 2;
+
+			printf("%s\n", text);
+			break;
+		case 3:
+			puts("Recived too many arguments!");
+			return 1;
+	}
+	return 0;
+}
+
+int uwuify(void) {
+	char *line = NULL;
+	size_t size = 0;
+	int ret = 0;
+
+	while (1) {
+		printf(">>> ");
+
+		size_t length = sof_getline(&line, &size, stdin);
+
+		if (length <= 0) {
+			ret = 3;
+			break;
+		}
+
+		if (line[0] == '\n' || line[0] == '\0') break;
+
+		char *uwuified = uwuify_string(line);
+		if (uwuified == NULL) {
+			ret = 2;
+			break;
+		}
+		printf("%s", uwuified);
+	}
+
+	free(line);
+	return ret;
+}
+
 // Found on stackoverflow (modifed it). This function was already in the public
 // domain, thought it did not have any comments and I changed some of the
 // formatting. Thanks Will Hartung!
@@ -56,55 +108,4 @@ size_t sof_getline(char **lineptr, size_t *n, FILE *stream) {
 	*n = size;
 
 	return p - bufptr - 1;
-}
-
-int uwuify(void) {
-	char *line = NULL;
-	size_t size = 0;
-	int ret = 0;
-
-	while (1) {
-		printf(">>> ");
-
-		size_t length = sof_getline(&line, &size, stdin);
-
-		if (length <= 0) {
-			ret = 3;
-			break;
-		}
-
-		if (line[0] == '\n' || line[0] == '\0') break;
-
-		char *uwuified = uwuify_string(line);
-		if (uwuified == NULL) {
-			ret = 2;
-			break;
-		}
-		printf("%s", uwuified);
-	}
-
-	free(line);
-	return ret;
-}
-
-char *uwuify_text(char *input) { return uwuify_string(input); }
-
-int main(int argc, char *argv[]) {
-	char *text = NULL;
-	switch (argc) {
-		case 0:
-		case 1:
-			return uwuify();
-			break;
-		case 2:
-			text = uwuify_text(argv[1]);
-			if (text == NULL) return 2;
-
-			printf("%s\n", text);
-			break;
-		case 3:
-			puts("Recived too many arguments!");
-			return 1;
-	}
-	return 0;
 }
