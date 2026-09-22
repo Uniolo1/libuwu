6f4073ad1883a8c52707d276f700cacb37861fa1
Uniolo1 <89284448+Uniolo1@users.noreply.github.com>
2026-09-22 19:49:34 -0400

    Renamed demo program from 'demo' to 'uwuify'
    Signed-off-by: Uniolo1 <89284448+Uniolo1@users.noreply.github.com>

--- temp_make.sh
+++ temp_make.sh
@@ -6,4 +6,4 @@
 gcc -Wall -Wextra -pedantic -O2 -c lib/*.c ; mv *.o out ; cd out ; ar rcs libuwuify.a *.o ; cd ..
 
 # demo
-gcc cmd/main.c -I. -Lout -luwuify -o out/demo --std=c99
+gcc cmd/main.c -I. -Lout -luwuify -o out/uwuify --std=c99
