0f011d09f7d248d3c69185441a336de8f06f9ba9
Uniolo1 <89284448+Uniolo1@users.noreply.github.com>
2026-09-21 20:28:47 -0400

    Added some more replacemnts
    Signed-off-by: Uniolo1 <89284448+Uniolo1@users.noreply.github.com>

--- src/parse.c
+++ src/parse.c
@@ -70,6 +70,8 @@ char *replace_string(const char *input) {
 	// WARNING: the length of the replacement must be <= that of the input
 	if (word_len == 4 && strncmp(input, "love", word_len) == 0)
 		replacement = "wuv";
+	if (word_len == 5 && strncmp(input, "loved", word_len) == 0)
+		replacement = "wuved";
 	else if (word_len == 5 && strncmp(input, "small", word_len) == 0)
 		replacement = "smol";
 	else if (word_len == 4 && strncmp(input, "this", word_len) == 0)
@@ -80,6 +82,10 @@ char *replace_string(const char *input) {
 		replacement = "boi";
 	else if (word_len == 5 && strncmp(input, "angry", word_len) == 0)
 		replacement = "angi";
+	else if (word_len == 3 && strncmp(input, "guh", word_len) == 0)
+		replacement = "buh";
+	else if (word_len == 2 && strncmp(input, ":)", word_len) == 0)
+		replacement = ":3";
 
 	if (!replacement) return NULL;
 
